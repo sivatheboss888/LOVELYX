a5d992adaa3c2065cce1ff31e8c2a8a949eb21c9
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-10-27 19:35:35 +0530

    Update chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -118,7 +118,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://www.kuki-api.tk/api/Kuki/MoeZilla/message='+Message)
+        kukiurl = requests.get('https://www.kuki-api.tk/KUKIoQzD5Gd53/Lovely/Tushar/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
