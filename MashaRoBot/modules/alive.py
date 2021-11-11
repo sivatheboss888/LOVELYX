bab8f201a0fd9d4542d44aa9d374441f378cc7ef
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-11-11 12:57:07 +0530

    Update alive.py

--- MashaRoBot/modules/alive.py
+++ MashaRoBot/modules/alive.py
@@ -1,16 +1,16 @@
 from telethon import events, Button, custom
 import re, os
 from MashaRoBot.events import register
-from MashaRoBot import telethn as tbot
+from MashaRoBot import telethn as love
 from MashaRoBot import telethn as tgbot
 PHOTO = "https://telegra.ph/file/63ec234e1be1f16345e69.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   LOVELY = event.sender.first_name
-  LOVELY = "**♡ I,m Lovely💕** \n\n"
-  LOVELY += "**♡ I'm Working with awesome speed**\n\n"
-  LOVELY += "**♡ Lovely : 2.0 LATEST**\n\n"
-  LOVELY += "**♡ My Creator :** [𝗧𝗨𝗦𝗛𝗔𝗥😍](t.me/Tushar204)\n\n"
-  LOVELY += "**♡ Telethon Version : 1.23.0**\n\n"
-  BUTTON = [[Button.url("𝐒𝐔𝐏𝐏𝐎𝐑𝐓🙂", "https://t.me/LOVELYAPPEAL"), Button.url("𝐔𝐏𝐃𝐀𝐓𝐄", "https://t.me/LOVELY_ROBOTS")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LOVELY,  buttons=BUTTON)
+  LOVELY = "**𝗪𝗛𝗢 𝗔𝗠 𝗜 🥺❓** \n\n"
+  LOVELY += "**I'ᴍ Lᴏᴠᴇʟʏ💞, Yᴏᴜʀ Hᴇᴀʀᴛʙᴇᴀᴛ❤️🙈*\n\n"
+  LOVELY += "**I'ᴍ Wᴏʀᴋɪɴɢ Oɴ Lᴏᴠᴇ 😜**\n\n"
+  LOVELY += "**Mʏ Lᴏᴠᴇ 🥰 :**  [ 🇷 ØΜΔŇŦIĆ❤️ 🇸 ĦΔ¥ΔŘ 🇹 ỮŞĦΔŘ](t.me/TUSHAR204)\n\n"
+  LOVELY += "**Aʙᴏᴜᴛ Mʏ Lᴏᴠᴇ 🤩 :** [「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳](t.me/ABOUTVEDMAT)**\n\n"
+  BUTTON = [[Button.url("𝗦𝗨𝗣𝗣𝗢𝗥𝗧🙂", "https://t.me/LOVELYAPPEAL"), Button.url("𝗨𝗣𝗗𝗔𝗧𝗘", "https://t.me/LOVELY_ROBOTS")]]
+  await love.send_file(event.chat_id, PHOTO, caption=LOVELY,  buttons=BUTTON)
