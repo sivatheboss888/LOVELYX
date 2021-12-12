559720a92149e8327830693aeb943a5fff8b078e
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-12 12:42:22 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -369,7 +369,7 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Emcee's licensed under the GNU General Public License v3.0_
+                 \n_Lovely's licensed under the GNU General Public License v3.0_
                  Here is the [💾Repository](https://t.me/LOVELYAPPEAL).
                  If you have any question about Lovely, let us know at @LOVELYAPPEAL.""",
             parse_mode=ParseMode.MARKDOWN,
