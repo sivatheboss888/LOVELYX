482591e0baf9582ebd508459b409e5e9455f5c76
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-16 14:34:30 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -387,12 +387,12 @@ def Source_about_callback(update: Update, context: CallbackContext):
     if query.data == "source_":
         query.message.edit_text(
             text=""" ℹ️ I'm *LOVELY*, a powerful group management bot built to help you manage your group easily.
-                 ❍ I can restrict users.
-                 ❍ I can greet users with customizable welcome messages and even set a group's rules.
-                 ❍ I have an advanced anti-flood system.
-                 ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
-                 ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
-                 ❍ I check for admins' permissions before executing any command and more stuffs
+❍ I can restrict users.
+❍ I can greet users with customizable welcome messages and even set a group's rules.
+❍ I have an advanced anti-flood system.
+❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
+❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
+❍ I check for admins' permissions before executing any command and more stuffs
                  \n_Lovely's licensed under the GNU General Public License v3.0_                 
                  If you have any question about Lovely, let us know at @LOVELYAPPEAL.""",                 
             parse_mode=ParseMode.MARKDOWN,
