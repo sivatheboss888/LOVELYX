479f1553e9b8157ee7e26503f86b14463d7f3a31
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-21 22:57:03 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -1337,8 +1337,6 @@ def main():
 
     dispatcher.add_handler(about_callback_handler)
 
-    dispatcher.add_handler(tushar_callback_handler)
-
     dispatcher.add_handler(owner_callback_handler)
 
     dispatcher.add_handler(settings_handler)
