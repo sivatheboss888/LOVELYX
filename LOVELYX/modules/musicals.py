27a62283d503f6b3d33f535ec8896caadba29e59
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-23 23:11:29 +0530

    Create musicals.py

---
+++ LOVELYX/modules/musicals.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Mᴜsɪᴄ🎶"
+
+__help__ = """
+➲ /audio <song name>*:* Uploads the Audio in it's best quality available
+ 💡Ex: `/audio Faded Alan Walker`
+➲ /song <song name>*:* Uploads the Audio in it's best quality available
+ 💡Ex: `/song teri mitti`
+➲ /saavn <song name>*:* Download song from saavn.
+📢𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 *:- @TEAM_LOV3LY ❤️‍🔥*
+"""
