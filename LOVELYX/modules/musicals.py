5564548b3300ad033f49382d3aa0ea9598b3f119
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-23 23:19:22 +0530

    Update musicals.py

--- LOVELYX/modules/musicals.py
+++ LOVELYX/modules/musicals.py
@@ -6,5 +6,5 @@ __help__ = """
 ➲ /song <song name>*:* Uploads the Audio in it's best quality available
  💡Ex: `/song teri mitti`
 ➲ /saavn <song name>*:* Download song from saavn.
-📢𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 *:- @TEAM_LOV3LY ❤️‍🔥*
+📢𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 **:- @TEAM_LOV3LY ❤️‍🔥**
 """
